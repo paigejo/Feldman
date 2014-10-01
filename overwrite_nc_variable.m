5adf50ff843ade22dba99db0c205925561e41b57
paigejo <paigejo@gmail.com>
2014-10-01 00:43:00 -0700

    added evalc in overwrite_nc_variable so there would be less unnecessary output

--- overwrite_nc_variable.m
+++ overwrite_nc_variable.m
@@ -17,6 +17,7 @@ function overwrite_nc_variable(fName, varName, newData, newName)
 %delete old variable
 delete_nc_variable(fName, varName);
 
-%make new variable with new data
-create_nc_variable(fName, newName, newData);
+%make new variable with new data (use evalc to not output way too much
+%data.  Note: this may cause errors to go unnoticed until later
+evalc('create_nc_variable(fName, newName, newData)');
 end

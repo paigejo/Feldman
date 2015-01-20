75da6557696015d80e0686a93ff5f5b431b69fce
paigejo <paigejo@gmail.com>
2015-01-20 12:05:04 -0800

    added search string for easier specification of files to send

--- PCA/scp_lou_to_remote.m
+++ PCA/scp_lou_to_remote.m
@@ -5,8 +5,10 @@
 %
 %e.g. of remoteStr:
 %'jpaige@edison.nersc.gov:/global/scratch2/sd/jpaige/PCA/all_sw/'
+%e.g. of searchStr:
+%'*.nc' or 'b30.036a.cam2.h0*.nc'
 
-function scp_lou_to_remote(remoteStr, pw)
+function scp_lou_to_remote(searchStr, remoteStr, pw)
 
 %{
 %get files
@@ -30,5 +32,5 @@ end
 %}
 
 %retrieve files from tape and send them all at once
-system('dmget *.nc');
-system(['scp *.nc ', remoteStr]);
+system(['dmget ', searchStr]);
+system(['scp ', searchStr, ' ', remoteStr]);

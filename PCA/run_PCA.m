1dd9aaebc0b08ee7cc576bac1ecf1e8f111d4bf1
paigejo <paigejo@gmail.com>
2015-01-28 16:47:32 -0800

    Created script equivalent to lou_run_PCA.  This runs on NERSC though

---
+++ PCA/run_PCA.m
@@ -0,0 +1,48 @@
+%this script performs all moving average PCA analysis on NERSC systems (for
+%all decades)
+
+function run_PCA(useSW, useLW)
+
+swPath = '/global/scratch2/sd/jpaige/PCA/sw_files/';
+lwPath = '/global/scratch2/sd/jpaige/PCA/lw_files/';
+
+dbstop if error;
+
+commandStr = '';
+if useSW
+    commandStr = 'sw';
+end
+if useLW
+    commandStr = [commandStr, 'lw'];
+end
+    
+
+for i = 0:9
+    
+    %decade data specification strings
+    iStr = [num2str(i), '9'];
+    iMMStr = [num2str(i-1), '9'];
+    
+    %no data from 1999, so must modify strings for first decade
+    if i == 0
+        iMMStr = '00';
+    end
+    
+    disp(['Running PCA code on 20', iMMStr, '-20', iStr, ' decade']);
+    
+    %determine inputs for PCA function based on decade
+    savePath = ['/global/scratch2/sd/jpaige/PCA/all_', commandStr, '/b30.042a.cam2/20', iMMStr, '-20', iStr, '/'];
+    saveName = ['PCA_', commandStr, '_MA_20', iMMStr, '-20', iStr, '.mat'];
+    
+    if i == 0
+        searchStr = 'b30.042a.cam2.h0.200*.nc';
+    else
+        searchStr = ['b30.042a.cam2.h0.20', iMMStr, '*.nc b30.042a.cam2.h0.20', iStr(1), '*.nc'];
+    end
+    
+    %run PCA code
+    lou_PCA_moving_avg(useSW, useLW, saveName, savePath, swPath, lwPath, searchStr)
+    
+end
+
+end

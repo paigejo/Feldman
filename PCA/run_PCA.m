5b7d27aada10fac4fed46fc85e87374b9437ce64
paigejo <paigejo@gmail.com>
2015-02-27 18:07:43 -0800

    fixed save path, search path, and search path strings

--- PCA/run_PCA.m
+++ PCA/run_PCA.m
@@ -5,6 +5,7 @@ function run_PCA(useSW, useLW, lwHiRes)
 
 swPath = '/global/scratch2/sd/jpaige/PCA/sw_files/';
 lwPath = '/global/scratch2/sd/jpaige/PCA/lw_files/';
+lwHiRes = logical(lwHiRes);
 
 dbstop if error;
 
@@ -13,32 +14,27 @@ if useSW
     commandStr = 'sw';
 end
 if useLW
-    commandStr = [commandStr, 'lw'];
+    commandStr = [commandStr, 'lw'];  
+end
+
+if lwHiRes
+    resStr = '_hres';
+else
+    resStr = '_lres';
 end
     
 
 for i = 0:9
     
     %decade data specification strings
-    iStr = [num2str(i), '9'];
-    iMMStr = [num2str(i-1), '9'];
+    decadeStr = [num2str(i)];
     
-    %no data from 1999, so must modify strings for first decade
-    if i == 0
-        iMMStr = '00';
-    end
-    
-    disp(['Running PCA code on 20', iMMStr, '-20', iStr, ' decade']);
+    disp(['Running PCA code on 20', decadeStr, '0-20', decadeStr, '9 decade']);
     
     %determine inputs for PCA function based on decade
-    savePath = ['/global/scratch2/sd/jpaige/PCA/all_', commandStr, '/b30.042a.cam2/20', iMMStr, '-20', iStr, '/'];
-    saveName = ['PCA_', commandStr, '_final_20', iMMStr, '-20', iStr, '.mat'];
-    
-    if i == 0
-        searchStr = 'b30.042a.cam2.h0.200*.nc';
-    else
-        searchStr = ['b30.042a.cam2.h0.20', iMMStr, '*.nc b30.042a.cam2.h0.20', iStr(1), '*.nc'];
-    end
+    savePath = ['/global/scratch2/sd/jpaige/PCA/all_', commandStr, '/b30.042a.cam2/20', decadeStr, '0-20', decadeStr, '9/'];
+    saveName = ['PCA_', commandStr, '_final_20', decadeStr, '0-20', decadeStr, '9', resStr, '.mat'];
+    searchStr = ['b30.042a.cam2.h0.20', decadeStr, '*.nc'];
     
     %run PCA code
     PCA_final(useSW, useLW, saveName, savePath, swPath, lwPath, searchStr, lwHiRes)

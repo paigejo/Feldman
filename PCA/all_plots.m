62242d9f07e079d365d2c33925ed60c21ddf5319
paigejo <paigejo@gmail.com>
2015-03-22 18:30:21 -0600

    fixed major typo

--- PCA/all_plots.m
+++ PCA/all_plots.m
@@ -2,11 +2,11 @@
 %plot for each PC:
 
 %set these options
-useSW = 0;
-useLW = 1;
+useSW = 1;
+useLW = 0;
 lwHiRes = 0;
 CLRorALL = 1; % 0 is clear-sky, 1 is all-sky
-doCalculations = 1; %if 1, calculates certain variables for plots.  If the calculations are already done set to 0.
+doCalculations = 0; %if 1, calculates certain variables for plots.  If the calculations are already done set to 0.
 
 %start code:
 
@@ -54,17 +54,17 @@ if doCalculations
             iMMStr = '00';
         end
         
-        fileName = ['PCA_', waveLStr, '_final_20', iMMStr, '-20', iStr, '_', resStr, '.mat'];
+        fileName = ['PCA_', waveLStr, '_cellnorm_20', iMMStr, '-20', iStr, '_', resStr, '.mat'];
         
         %load data, initialize variables if necessary:
         load(fileName);
         if i == 0
             nChannels = size(V, 1);
-            Vs = ones(nChannels, nPCs, nDecades);
-            lonLatScoreMats = ones(nLon, nLat, nTimeSteps, nPCs, nDecades);
-            variancesExplained = ones(nPCs, nDecades);
-            VEspace_all = ones(nLon, nLat, nPCs+1, nDecades);
-            VEtime_all = ones(nTimeSteps, nPCs+1, nDecades);
+            Vs = ones(nChannels, nPCs, nDecades)*NaN;
+            lonLatScoreMats = ones(nLon, nLat, nTimeSteps, nPCs, nDecades)*NaN;
+            variancesExplained = ones(nPCs, nDecades)*NaN;
+            VEspace_all = ones(nLon, nLat, nPCs+1, nDecades)*NaN;
+            VEtime_all = ones(nTimeSteps, nPCs+1, nDecades)*NaN;
         end
         
         %update variables with this decade's data
@@ -96,6 +96,95 @@ if doCalculations
     angles = min(angles, 180 - angles); %since we care about the linear
     %subspace spanned by the vectors, not the vectors' direction signs
     
+    %Calculate PC1-6 core trends as function of grid cell, decade
+    disp('Calculating score trend slopes')
+    scoreTrendSlopes = ones(nLon, nLat, nPCs, nDecades)*NaN;
+    for dec = 1:size(lonLatScoreMats, 5)
+        
+        for lon = 1:size(lonLatScoreMats, 1)
+            if mod(lon, 10) == 0
+                disp(['Current lon is: ', num2str(lon)]);
+            end
+            
+            for lat = 1:size(lonLatScoreMats, 2)
+                
+                for PC = 1:size(lonLatScoreMats, 4) %PC should go up to 6
+                    
+                    %get finite-valued score time series for this grid cell and PC
+                    timeSeries = squeeze(lonLatScoreMats(lon, lat, :, PC, dec));
+                    finite = isfinite(timeSeries);
+                    
+                    %if all data is NaN, do not calculate trend here
+                    if sum(finite) < 2
+                        scoreTrendSlopes(lon, lat, PC) = NaN;
+                        continue;
+                    end
+                    
+                    %enough data is finite, so calculate linear trend
+                    time = (1:length(timeSeries)).';
+                    finiteTrend = timeSeries;
+                    finiteTrend(~finite) = [];
+                    finiteTime = time;
+                    finiteTime(~finite) = [];
+                    
+                    %fit and save trend slope from data for this grid cell and
+                    %channel
+                    linCoeffs = polyfit(finiteTime, finiteTrend, 1);
+                    scoreTrendSlopes(lon, lat, PC, dec) = linCoeffs(1); %first element is linear coef, second is constant coef
+                    
+                end
+            end
+        end
+    end
+    
+    %Calculate if PC1-6 score trend detection time as function of grid cell assuming AR(1) model
+    disp('Calculating score trend detection times')
+    trendDetectionTimes = ones(size(scoreTrendSlopes))*NaN;
+    for dec = 1:size(lonLatScoreMats, 5)
+        
+        for lon = 1:size(lonLatScoreMats, 1)
+            if mod(lon, 10) == 0
+                disp(['Current lon is: ', num2str(lon)]);
+            end
+            
+            for lat = 1:size(lonLatScoreMats, 2)
+                
+                for PC = 1:size(lonLatScoreMats, 4) %PC should go up to 6
+                    
+                    %get finite-valued score time series for this grid cell and PC
+                    timeSeries = squeeze(lonLatScoreMats(lon, lat, :, PC, dec));
+                    finite = isfinite(timeSeries);
+                    
+                    %if too much data is NaN, do not calculate trend here
+                    if sum(finite) < 2
+                        trendDetectionTimes(lon, lat, PC, dec) = NaN;
+                        continue;
+                    end
+                    
+                    %otherwise, remove non-finite data
+                    time = (1:length(timeSeries)).';
+                    finiteTrend = timeSeries;
+                    finiteTrend(~finite) = [];
+                    finiteTime = time;
+                    finiteTime(~finite) = [];
+                    
+                    %calculate trend detection time (assuming AR(1) model)
+                    var_m = 0; %measurement error
+                    [t_detect,~] = return_detection_time_two_timeseries2( ...
+                        finiteTrend,zeros(size(finiteTrend)),finiteTime,var_m);
+                    trendDetectionTimes(lon, lat, PC, dec) = t_detect;
+                    
+                end
+            end
+        end
+    end
+    trendDetectionTimes(~isfinite(trendDetectionTimes)) = NaN; %set all infinite values to NaNs
+    
+    %permute lon and lat for imagesc plots
+    scoreTrendSlopes = permute(scoreTrendSlopes, [2 1 3 4]);
+    trendDetectionTimes = permute(trendDetectionTimes, [2 1 3 4]);
+    VEspace_all = permute(VEspace_all, [2 1 3 4]);
+
 end
 
 clim = [0 max(angles(:))+max(angles(:))/254]; %add to max so there is slight difference between max and no data color
@@ -164,212 +253,122 @@ xlabel('Principle Component');
 ylabel('Fraction of Variance Explained');
 set(gca, 'XTickLabel', {'', '1', '2', '3', '4', '5', '6', ''})
 
-%Calculate PC1-6 core trends as function of grid cell, decade
-disp('Calculating score trend slopes')
-scoreTrendSlopes = ones(nLon, nLat, nPCs, nDecades);
-for dec = 1:size(lonLatScoreMats, 5)
-    
-    for lon = 1:size(lonLatScoreMats, 1)
-        if mod(lon, 10) == 0
-            disp(['Current lon is: ', num2str(lon)]);
-        end
-        
-        for lat = 1:size(lonLatScoreMats, 2)
-            
-            for PC = 1:size(lonLatScoreMats, 4) %PC should go up to 6
-                
-                %get finite-valued score time series for this grid cell and PC
-                timeSeries = squeeze(lonLatScoreMats(lon, lat, :, PC, dec));
-                finite = isfinite(timeSeries);
-                
-                %if all data is NaN, do not calculate trend here
-                if sum(finite) < 2
-                    scoreTrendSlopes(lon, lat, PC) = NaN;
-                    continue;
-                end
-                
-                %enough data is finite, so calculate linear trend
-                time = (1:length(timeSeries)).';
-                finiteTrend = timeSeries;
-                finiteTrend(~finite) = [];
-                finiteTime = time;
-                finiteTime(~finite) = [];
-                
-                %fit and save trend slope from data for this grid cell and
-                %channel
-                linCoeffs = polyfit(finiteTime, finiteTrend, 1);
-                scoreTrendSlopes(lon, lat, PC, dec) = linCoeffs(1); %first element is linear coef, second is constant coef
-                
-            end
-        end
-    end
-end
-scoreTrendSlopes = permute(scoreTrendSlopes, [2 1 3 4]); %permute so that lon is x and lat is y on image plots
-
-%Plot PC1-6 score trend slopes as function of grid cell
+%Plot PC1-6 avg score trend slopes as function of grid cell
 cmap = jet(256);
-for dec = 1:nDecades
-    for PC = 1:nPCs %PC should go up to 6
-        
-        slopesToPlot = scoreTrendSlopes(:, :, PC, dec);
-        
-        clim = [min(slopesToPlot(:)) max(slopesToPlot(:))];
-        figure;
-        a = imagesc(slopesToPlot, clim);
-        colormap(cmap);
-        set(gca,'YDir','normal');
-        colorbar();
-        title(['Slope of Trend in PC ', num2str(PC) ' Scores for Decade ', num2str(dec), ' ', titleString]);
-        set(a,'AlphaData',~isnan(slopesToPlot));
-        
-    end
-end
-
-%Calculate if PC1-6 score trend detection time as function of grid cell assuming AR(1) model
-disp('Calculating score trend detection times')
-trendDetectionTimes = ones(size(scoreTrendSlopes));
-for dec = 1:size(lonLatScoreMats, 5)
+for PC = 1:nPCs %PC should go up to 6
     
-    for lon = 1:size(lonLatScoreMats, 1)
-        if mod(lon, 10) == 0
-            disp(['Current lon is: ', num2str(lon)]);
-        end
-        
-        for lat = 1:size(lonLatScoreMats, 2)
-            
-            for PC = 1:size(lonLatScoreMats, 4) %PC should go up to 6
-                
-                %get finite-valued score time series for this grid cell and PC
-                timeSeries = squeeze(lonLatScoreMats(lon, lat, :, PC, dec));
-                finite = isfinite(timeSeries);
-                
-                %if too much data is NaN, do not calculate trend here
-                if sum(finite) < 2
-                    trendDetectionTimes(lon, lat, PC, dec) = NaN;
-                    continue;
-                end
-                
-                %otherwise, remove non-finite data
-                time = (1:length(timeSeries)).';
-                finiteTrend = timeSeries;
-                finiteTrend(~finite) = [];
-                finiteTime = time;
-                finiteTime(~finite) = [];
-                
-                %calculate trend detection time (assuming AR(1) model)
-                var_m = 0; %measurement error
-                [t_detect,~] = return_detection_time_two_timeseries2( ...
-                    finiteTrend,zeros(size(finiteTrend)),finiteTime,var_m);
-                trendDetectionTimes(lon, lat, PC, dec) = t_detect;
-                
-            end
-        end
-    end
+    avgSlopes = nanmean(scoreTrendSlopes(:, :, PC, :), 4);
+    
+    clim = [min(avgSlopes(:)) max(avgSlopes(:))];
+    figure;
+    a = imagesc(avgSlopes, clim);
+    colormap(cmap);
+    set(gca,'YDir','normal');
+    colorbar();
+    title(['Average PC ', num2str(PC) ' Score Trend Slope ', titleString]);
+    set(a,'AlphaData',~isnan(avgSlopes));
+    
 end
-trendDetectionTimes = permute(trendDetectionTimes, [2 1 3 4]); %permute so that lon is x and lat is y on image plots
 
 %Plot PC1-6 score trend detection times as function of grid cell and
 %channel
-high = log10(max(trendDetectionTimes(:))/12);
-low = log10(min(trendDetectionTimes(:))/12);
+finiteTrendDetectionTimes = trendDetectionTimes(isfinite(trendDetectionTimes));
+high = max(finiteTrendDetectionTimes(:))/12;
+low = min(finiteTrendDetectionTimes(:))/12;
 range = high - low;
 clim = [low-range/253 high];
 cmap = jet(256);
-for dec = 1:size(trendDetectionTimes, 4)
-    for PC = 1:size(trendDetectionTimes, 3) %PC should go up to 6
-        
-        figure;
-        a = imagesc(log10(trendDetectionTimes(:, :, PC, dec)/12), clim);
-        colormap(cmap);
-        set(gca,'YDir','normal');
-        colorbar();
-        title(['AR(1) Trend Detection Time (in Log10 Years) for PC ', num2str(PC) ' Scores for Decade ', num2str(dec), ' ', titleString]);
-        set(a,'AlphaData',~isnan(trendDetectionTimes(:, :, PC, dec)));
-        
-    end
+for PC = 1:size(finiteTrendDetectionTimes, 3) %PC should go up to 6
+    
+    avgTrendDetectionTimes = nanmean(finiteTrendDetectionTimes(:, :, PC, :)/12, 4);
+    
+    figure;
+    a = imagesc(avgTrendDetectionTimes, clim);
+    colormap(cmap);
+    set(gca,'YDir','normal');
+    colorbar();
+    title(['AR(1) Average Trend Detection Time for PC ', num2str(PC) ' Scores ', titleString]);
+    set(a,'AlphaData',~isnan(avgTrendDetectionTimes));
+    
 end
 
 %Plot PC1-6 score trend detection times as function of grid cell and
 %channel, but only if detection time is bigger than length of dataset (all
 %other detection times plotted as black)
-logDetectYears = log10(trendDetectionTimes/12);
-high = max(logDetectYears(:));
-low = log10(100);
+detectYears = trendDetectionTimes/12;
+high = max(detectYears(:));
+low = 100;
 clim = [low high];
-significant = logDetectYears <= low;
-logDetectYears(significant) = -Inf; %significant trend cells should be black so set to black (bottom color of customJet)
+significant = detectYears <= low;
+detectYears(significant) = -Inf; %significant trend cells should be black so set to black (bottom color of customJet)
 cmap = customJet; % the first color of this color map is black, and colors are less dark than in jet
-for dec = 1:nDecades
-    for PC = 1:size(logDetectYears, 3) %PC should go up to 6
-        
-        figure;
-        a = imagesc(logDetectYears(:, :, PC, dec), clim);
-        colormap(cmap)
-        set(gca,'YDir','normal');
-        bar = colorbar();
-        title(['AR(1) Trend Detection Time (in Years) for PC ', num2str(PC) ' Scores for Decade ', num2str(dec), ': Undetectable Times ', titleString]);
-        set(a,'AlphaData',~isnan(logDetectYears(:, :, PC, dec)));
-        ytick = get(bar, 'YTick');
-        set(bar, 'YTickLabel', 10.^ytick);
-        
-    end
+for PC = 1:size(detectYears, 3) %PC should go up to 6
+    
+    avgDetectYears = nanmean(detectYears(:, :, PC, :), 4);
+    
+    figure;
+    a = imagesc(avgDetectYears, clim);
+    colormap(cmap)
+    set(gca,'YDir','normal');
+    bar = colorbar();
+    title(['Average AR(1) Trend Detection Time (in Years) for PC ', num2str(PC) ' Scores: Undetectable Times ', titleString]);
+    set(a,'AlphaData',~isfinite(avgDetectYears));
+    ytick = get(bar, 'YTick');
+    
 end
 
 %Plot PC1-6 score trend detection times as function of grid cell and
 %channel, but only if detection time is smaller than length of dataset (all
 %other detection times plotted as black)
-logDetectYears = log10(trendDetectionTimes/12);
-high = log10(100);
-low = min(logDetectYears(:));
+detectYears = trendDetectionTimes/12;
+high = 100;
+low = min(detectYears(:));
 clim = [low high];
-significant = logDetectYears <= high;
-logDetectYears(~significant) = -Inf; %insignificant trend cells should be black so set to black (bottom color of customJet)
+significant = detectYears <= high;
+detectYears(~significant) = -Inf; %insignificant trend cells should be black so set to black (bottom color of customJet)
 cmap = customJet; % the first color of this color map is black, and colors are less dark than in jet
-for dec = 1:nDecades
-    for PC = 1:size(logDetectYears, 3) %PC should go up to 6
-        
-        figure;
-        a = imagesc(logDetectYears(:, :, PC, dec), clim);
-        colormap(cmap)
-        set(gca,'YDir','normal');
-        bar = colorbar();
-        title(['AR(1) Trend Detection Time (in Years) for PC ', num2str(PC) ' Scores for Decade', num2str(dec), ': Detectable Times ', titleString]);
-        set(a,'AlphaData',~isnan(logDetectYears(:, :, PC, dec)));
-        ytick = get(bar, 'YTick');
-        set(bar, 'YTickLabel', 10.^ytick);
-        
-    end
+for PC = 1:size(detectYears, 3) %PC should go up to 6
+    
+    avgDetectYears = nanmean(detectYears(:, :, PC, :), 4);
+    
+    figure;
+    a = imagesc(avgDetectYears, clim);
+    colormap(cmap)
+    set(gca,'YDir','normal');
+    bar = colorbar();
+    title(['Average AR(1) Trend Detection Time (in Years) for PC ', num2str(PC) ' Scores: Detectable Times ', titleString]);
+    set(a,'AlphaData',~isnan(avgDetectYears));
+    ytick = get(bar, 'YTick');
+    
 end
 
 %Variance explaned plots go here
 cmap = jet(256);
-for dec = 1:nDecades
-    for PC = 1:(nPCs+1)
-        
-        
-        PCstr = ['PC ', num2str(PC)];
-        if PC == 7
-            PCstr = 'First Six PCs';
-        end
-        
-        %spatial plot
-        tmp_space = VEspace_all(:, :, PC, dec);
-        figure;
-        a = imagesc(tmp_space);
-        colormap(cmap);
-        set(gca,'YDir','normal');
-        colorbar();
-        title(['Variance Explained by ', num2str(PC) ' for Decade ', num2str(dec), ' ', titleString]);
-        set(a,'AlphaData',~isnan(tmp_space));
-        
-        %temporal plot
-        tmp_time = VEtime_all(:, PC, dec);
-        figure;
-        plot(tmp_time);
-        title(['Variance Explained by ', num2str(PC) ' for Decade ', num2str(dec), ' ', titleString]);
-        xlabel('Timestep (Months)');
-        
+for PC = 1:(nPCs+1)
+    
+    
+    PCstr = ['PC ', num2str(PC)];
+    if PC == 7
+        PCstr = 'First Six PCs';
     end
+    
+    %spatial plot
+    tmp_space = nanmean(VEspace_all(:, :, PC, :), 4);
+    figure;
+    a = imagesc(tmp_space);
+    colormap(cmap);
+    set(gca,'YDir','normal');
+    colorbar();
+    title(['Average Variance Explained by ', PCstr ' ', titleString]);
+    set(a,'AlphaData',~isnan(tmp_space));
+    
+    %temporal plot
+    tmp_time = nanmean(VEtime_all(:, PC, :), 3);
+    figure;
+    plot(tmp_time);
+    title(['Average Variance Explained by ', PCstr ' ', titleString]);
+    xlabel('Timestep (Months)');
+    
 end
 
 %SPE plots go here
@@ -388,7 +387,7 @@ for i = 0:(nDecades-1)
     fileName = ['PCA_', waveLStr, '_final_', inString, '20', iMMStr, '-20', iStr, '.mat']; 
     
     %load data:
-    load(fileName, 'SPEspacetime', 'SPEspectrum');
+    load(fileName, 'avgSPEspace', 'avgSPEspectrum', 'avgSPEtime');
     
     for PC = 1:(nPCs+1)
         
@@ -399,11 +398,10 @@ for i = 0:(nDecades-1)
         end
         
         figure;
-        avgSPEspectrum = SPEspectrum/(nLon*nLat*nTimeSteps);
         if useSW && useLW
             plot(waveNum(1:nSW), SPEspectrum(1:nSW, i+1), 'b');
             hold on;
-            plot(waveNum((nSW+1):end), SPEspectrum((nSW+1):end, i+1), 'r'); %206 696 3304
+            plot(waveNum((nSW+1):end), SPEspectrum((nSW+1):end, i+1), 'r');
             xlabel('Wavelength (nm), Wavenumber (cm^{-1})');
             hold off;
             
@@ -420,10 +418,16 @@ for i = 0:(nDecades-1)
         
         %spatial error plots
         figure;
-        avgSPEspatial = nanmean(SPEspacetime(:, :, :, PC), 3);
-        imagesc(avgSPEspatial);
+        imagesc(avgSPEspace(:, :, i));
         colobar();
         title(['Mean SPE For ', PCstr, ' in the ', iMMstr, '''s ', titleString]);
+        
+        %temporal error plots
+        figure;
+        plot((1:length(avgSPEtime))/12, avgSPEtime(:, PC));
+        xlabel('Time (Years)');
+        ylabel('SPE');
+        title('SPE Through Time');
         
     end
     
